d96e2c4d6d458c9be7e748d77c68b2220530b1c4
Max Hilken <hilkenmc@gmail.com>
2022-02-18 16:51:00 -0500

    Start of project

---
+++ src/main/java/model/Menu.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Menu {
+/* What do we want in Menu?:
+-We want a header to welcome the customer to the store (banner)
+--printHeader()
+-We want to print the main menu that lets the user know what their choices are
+--printMenu()
+-We need a scanner that looks for the customers response to the main menu prompt
+--getInput()
+-We want a method to ensure input from customer is acceptable and within the menu prompt parameters
+--checkInput()
+We probably want to output a confirmation of sorts to the customer
+ */
+
+
+}
